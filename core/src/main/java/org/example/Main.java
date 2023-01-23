c6844c1d3d080695902d6539baa8c3d4d6723b33
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-01-23 16:52:39 +0330

    5- create bean confing file and one sample bean

--- core/src/main/java/org/example/Main.java
+++ core/src/main/java/org/example/Main.java
@@ -1,2 +1,31 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.domain.NumberGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class Main {
+
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+    private final static String  BEAN_CONFIG_FILE_LOCATION = "beans.xml";
+
+
+    public static void main(String[] args) {
+        log.info("guess Number game");
+
+        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(BEAN_CONFIG_FILE_LOCATION);
+
+        NumberGenerator numberGenerator = applicationContext.getBean("numberGenerator",NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        log.info("Number : {}",number);
+
+        applicationContext.close();
+    }
 }
