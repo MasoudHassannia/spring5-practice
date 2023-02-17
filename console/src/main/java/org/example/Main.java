6467ffd6ae7669cd59be4b5b4ab3c580d609efa8
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-02-17 16:55:13 +0330

    20- add spring boot config

--- console/src/main/java/org/example/Main.java
+++ console/src/main/java/org/example/Main.java
@@ -2,19 +2,19 @@ package org.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.config.JavaAnnotationConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         log.info("guess Number game");
 
-        ConfigurableApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(JavaAnnotationConfig.class);
-
-        applicationContext.close();
+        SpringApplication.run(Main.class,args);
     }
 
     // private final static String  BEAN_CONFIG_FILE_LOCATION = "beans.xml";
