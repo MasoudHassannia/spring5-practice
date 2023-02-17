c3b26ba0e889f58cdfaa0548aaff840bc754be28
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-02-17 17:25:53 +0330

    21- create web module for spring boot

--- console/src/main/java/org/example/Main.java
+++ console/src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.config.JavaAnnotationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 @Slf4j
 @SpringBootApplication
