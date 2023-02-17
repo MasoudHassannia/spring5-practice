634262337a1e44d58dd8d2d10e8fb4c4f0ad7881
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-02-17 23:32:09 +0330

    24- add devTools config

--- web/src/main/java/org/example/WebMain.java
+++ web/src/main/java/org/example/WebMain.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import com.sun.tools.javac.Main;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
