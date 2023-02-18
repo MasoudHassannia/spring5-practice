e3a0e14943b29356588da3980ffdc02202bff331
Masoud Hassannia <m.hasan.nia@sain.ir>
2023-02-19 01:30:11 +0330

    32- add request Interceptor to project

--- web/src/main/java/org/example/config/WebConfig.java
+++ web/src/main/java/org/example/config/WebConfig.java
@@ -1,7 +1,9 @@
 package org.example.config;
 
+import org.example.interceptor.RequestInterceptor;
 import org.example.util.ViewsNameConstants;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewsNameConstants.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
